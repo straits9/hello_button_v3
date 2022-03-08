f401c58c639f5c7e29e76e111b2606eba0ef1222
Paul Choi <straits9@gmail.com>
2022-03-08 19:40:52 +0900

    server part를 serverless로 구성. test deploy함.

---
+++ serverless/api/get_buttons.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var mysql = require('mysql');
+var pool_mariadb = mysql.createPool({
+	host				:'hf-dev.c1dsfak07rhb.ap-northeast-2.rds.amazonaws.com',
+	port				: 3306,
+	user				: 'hellobell',
+	password			: 'gntlr?znzl',
+	database			:'hellobell_v2',
+	connectionLimit		: 100,
+	//connectTimeout      : 60 * 60 * 1000,
+	//aquireTimeout       : 60 * 60 * 1000,
+	//timeout             : 60 * 60 * 1000,
+	waitForConnections	: true,
+	multipleStatements	: true
+});
+
+module.exports.buttons = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
